ba78b6114beb2849541f7bd714160f2b83abeffd
clxx <Carsten.Lindner@gmx.net>
2026-07-01 01:08:44 +0200

    update

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,4 +6,15 @@ def emotion_detector(text_to_analyse):
     myobj = {"raw_document": {"text": text_to_analyse}}
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json=myobj, headers=header)
-    return response.text
+    formatted_response = json.loads(response)
+    print(formatted_response)
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']["anger"]
+    return {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score,
+        "dominant_emotion": '<name of the dominant emotion>'
+    }
+    # return response.text

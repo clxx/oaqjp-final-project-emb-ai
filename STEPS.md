b1f3ba14b8a2fb6b414bcccb6f7be4ee0dae8268
clxx <Carsten.Lindner@gmx.net>
2026-07-07 23:32:42 +0200

    Update

--- STEPS.md
+++ STEPS.md
@@ -3,3 +3,7 @@
 `git clone https://github.com/clxx/oaqjp-final-project-emb-ai.git final_project`
 
 `python3 -m pip install requests`
+
+`python3 -m pip install pylint`
+
+`pylint server.py`
--- server.py
+++ server.py
@@ -16,7 +16,19 @@ def emo_detector():
     dominant_emotion = response["dominant_emotion"]
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
-    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+    return f"For the given statement, the system response is 'anger': {
+            anger
+        }, 'disgust': {
+            disgust
+        }, 'fear': {
+            fear
+        }, 'joy': {
+            joy
+        } and 'sadness': {
+            sadness
+        }. The dominant emotion is {
+            dominant_emotion
+        }."
 
 
 @app.route("/")
